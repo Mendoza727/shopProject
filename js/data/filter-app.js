7e120f5b94bac499fcea3005a699e1665b23a405
Juan <juancamilomendozavillegas14@gmail.com>
2023-02-15 08:18:03 -0500

    update js-filter and add options for filter

---
+++ js/data/filter-app.js
@@ -0,0 +1,202 @@
+export const typesFood = [
+
+    //Hamburgesas
+    {
+        id: 1,
+        type: 'hamburgesas',
+        name: 'Hamburgesa de carne',
+        img: 'assets/img/comida/comida.png',
+        descripcion: 'comida del suroriente',
+        estrellas: 3,
+        precio: 80000
+    },
+    {
+        id: 2,
+        type: 'hamburgesas doble carne',
+        name: 'comida1',
+        img: 'assets/img/comida/comida.png',
+        descripcion: 'comida del suroriente',
+        estrellas: 4,
+        precio: 920000
+    },
+    {
+        id: 3,
+        type: 'hamburgesas',
+        name: 'comida1',
+        img: 'assets/img/comida/comida.png',
+        descripcion: 'comida del suroriente',
+        estrellas: 5,
+        precio: 20000
+    },
+    {
+        id: 4,
+        type: 'hamburgesas',
+        name: 'comida1',
+        img: 'assets/img/comida/comida.png',
+        descripcion: 'comida del suroriente',
+        estrellas: 4,
+        precio: 20000
+    },
+
+    //pizzas
+
+    {
+        id: 5,
+        type: 'pizzas',
+        name: 'comida1',
+        img: 'assets/img/comida/comida.png',
+        descripcion: 'comida del suroriente',
+        estrellas: 4,
+        precio: 20000
+    },
+    {
+        id: 6,
+        type: 'pizzas',
+        name: 'comida1',
+        img: 'assets/img/comida/comida.png',
+        descripcion: 'comida del suroriente',
+        estrellas: 4,
+        precio: 20000
+    },
+    {
+        id: 7,
+        type: 'pizzas',
+        name: 'comidaOtroArray',
+        img: null,
+        descripcion: null,
+        estrellas: 2,
+        precio: 20000
+    },
+    {
+        id: 8,
+        type: 'pizzas',
+        name: 'comidaOtroArray',
+        img: null,
+        descripcion: null,
+        estrellas: 2,
+        precio: 20000
+    },
+    {
+        id: 9,
+        type: 'pizzas',
+        name: 'comidaOtroArray',
+        img: null,
+        descripcion: null,
+        estrellas: 2,
+        precio: 20000
+    },
+
+    //patatas
+    
+    {
+        id: 10,
+        type: 'patatas',
+        name: 'comidaOtroArray',
+        img: null,
+        descripcion: null,
+        estrellas: 2,
+        precio: 20000
+    },
+    {
+        id: 11,
+        type: 'patatas',
+        name: 'comidaOtroArray',
+        img: null,
+        descripcion: null,
+        estrellas: 2,
+        precio: 20000
+    },
+
+    //pollo
+
+    {
+        id: 12,
+        type: 'pollo',
+        name: 'comidaOtroArray',
+        img: null,
+        descripcion: null,
+        estrellas: 2,
+        precio: 20000
+    },
+    {
+        id: 13,
+        type: 'pollo',
+        name: 'comidaOtroArray',
+        img: null,
+        descripcion: null,
+        estrellas: 2,
+        precio: 20000
+    },
+
+    //tacos
+
+        {
+            id: 14,
+            type: 'tacos',
+            name: 'comidaOtroArray',
+            img: null,
+            descripcion: null,
+            estrellas: 2,
+            precio: 20000
+        },
+        {
+            id: 15,
+            type: 'tacos',
+            name: 'comidaOtroArray',
+            img: null,
+            descripcion: null,
+            estrellas: 2,
+            precio: 20000
+        },
+
+    //empanadas
+        {
+            id: 16,
+            type: 'empanada',
+            name: 'empanadas',
+            img: null,
+            descripcion: 'empanadas rico',
+            estrellas: 5,
+            precio: 2000
+        },
+        {
+            id: 17,
+            type: 'empanada',
+            name: 'empanadas',
+            img: null,
+            descripcion: 'empanadas rico',
+            estrellas: 5,
+            precio: 2000
+        },
+        {
+            id: 18,
+            type: 'empanada',
+            name: 'empanadas',
+            img: null,
+            descripcion: 'empanadas rico',
+            estrellas: 5,
+            precio: 2000
+        },
+
+        //quesadillas
+        {
+            id: 19,
+            type: 'quesadillas',
+            name: 'quesadilla',
+            img: null,
+            descripcion: 'empanadas rico',
+            estrellas: 5,
+            precio: 20000
+        },
+        {
+            id: 20,
+            type: 'quesadillas',
+            name: 'quesadilla',
+            img: null,
+            descripcion: 'empanadas rico',
+            estrellas: 5,
+            precio: 200000
+        },
+
+
+]
---
+++ js/data/filter-options-app.js
@@ -0,0 +1,26 @@
+const options = [
+    {
+        option1: 'hamburgesa'   
+    },
+    {
+        option2: 'pizza'
+    },
+    {
+        option3: 'patatas'
+    },
+    {
+        option4: 'pollo'
+    },
+    {
+        option5: 'tacos'
+    },
+    {
+        option6: 'empanadas'
+    },
+    {
+        option7: 'quesadillas'
+    },
+    {
+        option8: 'all'
+    }
+];
